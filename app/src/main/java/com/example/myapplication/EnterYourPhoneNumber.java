ea76fcd426e25d519ff9304cebb03c9c90d4aff3
mattheu <mattheuhtn@gmail.com>
2022-04-05 08:02:23 +0700

    create interface login, logup, home (message, contact)

--- app/src/main/java/com/example/myapplication/EnterYourPhoneNumber.java
+++ app/src/main/java/com/example/myapplication/EnterYourPhoneNumber.java
@@ -6,18 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class EnterYourPhoneNumber extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        
+        setContentView(R.layout.activity_enter_your_phone_number);
     }
 
-    public void agreeClick(View view) {
-        Intent intent = new Intent(this, EnterYourPhoneNumber.class);
+    public void doneClick(View view) {
+        Intent intent = new Intent(this, Home.class);
         startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -0,0 +1,59 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class Home extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener(){
+
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                View person = (View) findViewById(R.id.contact);
+                View message = (View) findViewById(R.id.message);
+                switch (id) {
+                    case R.id.itemPerson:
+                        person.setVisibility(View.VISIBLE);
+                        message.setVisibility(View.GONE);
+                        return true;
+
+                    case R.id.itemHome:
+                        person.setVisibility(View.GONE);
+                        message.setVisibility(View.VISIBLE);
+                        return true;
+
+                }
+                return true;
+            }
+        });
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.bottom_nav_menu, menu);
+        return true;
+    }
+
+
+}
